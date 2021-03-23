14aecae8923c50186b8153a446b6dde887495ab1
yassiniyo2013 <59447804+yassiniyo2013@users.noreply.github.com>
2021-03-23 14:18:46 +0100

    Revert on click buy

--- html/script.js
+++ html/script.js
@@ -198,14 +198,16 @@
 						if (item != null) {
 							if ((item.name).split("_")[0] == "WEAPON" && item.metadata.durability !== undefined || null) {
 								$(".inventory-main-rightside").find("[inventory-slot=" + item.slot + "]").html('<div class="item-slot-img"><img src="images/' + item.name + '.png'+'" alt="' + item.name + '" /></div><div class="item-slot-count"><p>' + item.count + ' (' + (item.price) + '$)</p></div><div class="item-slot-label"><p><div class="item-slot-durability"><div class="item-slot-durability-bar"><p>100</p></div></div><div class="item-slot-label"><p>' + item.label + '</p></div></p></div>');
-								$(".inventory-main-rightside").find("[inventory-slot=" + item.slot + "]").data("ItemData", item).data("location", data.rightinventory.name);
-								//$(".inventory-main-rightside").find("[inventory-slot=" + item.slot + "]").addClass("drag-item");
+								//$(".inventory-main-rightside").find("[inventory-slot=" + item.slot + "]").data("ItemData", item).data("location", data.rightinventory.name);
+								$(".inventory-main-rightside").find("[inventory-slot=" + item.slot + "]").data("ItemData", item);
+								$(".inventory-main-rightside").find("[inventory-slot=" + item.slot + "]").addClass("drag-item");
 									var durability = HSN.InventoryGetDurability(item.metadata.durability)
 									$(".inventory-main-rightside").find("[inventory-slot=" + item.slot + "]").find(".item-slot-durability-bar").css({"background-color": durability[0],"width":durability[2]}).find('p').html(durability[1]);
 							} else {
 								$(".inventory-main-rightside").find("[inventory-slot=" + item.slot + "]").html('<div class="item-slot-img"><img src="images/' + item.name + '.png'+'" alt="' + item.name + '" /></div><div class="item-slot-count"><p>' + item.count + ' (' + (item.price) + '$)</p></div><div class="item-slot-label"><p><div class="item-slot-label"><p>' + item.label + '</p></div></p></div>');
-								$(".inventory-main-rightside").find("[inventory-slot=" + item.slot + "]").data("ItemData", item).data("location", data.rightinventory.name);
-								//$(".inventory-main-rightside").find("[inventory-slot=" + item.slot + "]").addClass("drag-item");
+								//$(".inventory-main-rightside").find("[inventory-slot=" + item.slot + "]").data("ItemData", item).data("location", data.rightinventory.name);
+								$(".inventory-main-rightside").find("[inventory-slot=" + item.slot + "]").data("ItemData", item);
+								$(".inventory-main-rightside").find("[inventory-slot=" + item.slot + "]").addClass("drag-item");
 							}
 						}
 					})
@@ -313,7 +315,7 @@
 		});
 	}
 
-	$(document).on("click", ".ItemBoxes", function(e){
+	/*$(document).on("click", ".ItemBoxes", function(e){
 		if ($(this).data("location") !== undefined || null) {
 			e.preventDefault();
 			var Item = $(this).data("ItemData")
@@ -327,7 +329,7 @@
 				}));
 			}
 		}
-	})
+	})*/
 
 
 	$(document).on("mouseenter", ".ItemBoxes", function(e){
